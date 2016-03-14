a5f31f9b44a3459e692958a68b389c0fc51d57ea
Dariusz Walczak <dariusz.w84@gmail.com>
2016-03-14 18:48:48 +0000

    Update makingChange.py

--- makingChange.py
+++ makingChange.py
@@ -21,7 +21,7 @@ def makingChange(a):
     import numpy as np
     x = 100 * np.array([100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01])
     y = np.array([])
-    a = a * 100
+    a = round(100 * (a * 100) / 100)
     i = 0
     j = 0
 
